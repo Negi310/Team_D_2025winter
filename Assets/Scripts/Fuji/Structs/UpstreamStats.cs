a0a0f0dd55d089e11d1bd526521f78c01e92a276
yokotani-cgp <s2510308.yokotani.cgp@gmail.com>
2026-03-06 22:59:11 +0900

    エラー除去
    ステータスのとこにコンストラクタ追加した

--- Assets/Scripts/Fuji/Structs/UpstreamStats.cs
+++ Assets/Scripts/Fuji/Structs/UpstreamStats.cs
@@ -5,4 +5,12 @@ public struct UpstreamStats
     public float Jump;
     public float Cautiousness; // 警戒心
     public float Stamina;
+
+    public UpstreamStats(float power, float jump, float cautiousness, float stamina)
+    {
+        Power = power;
+        Jump = jump;
+        Cautiousness = cautiousness;
+        Stamina = stamina;
+    }
 }

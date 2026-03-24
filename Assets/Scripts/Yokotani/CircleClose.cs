316a9a3917c9e626d14957452e7891b701f2ac9c
yokotani-cgp <s2510308.yokotani.cgp@gmail.com>
2026-03-25 02:44:31 +0900

    shadergraph

---
+++ Assets/Scripts/Yokotani/CircleClose.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class CircleClose : MonoBehaviour
+{
+    public Material mat;
+    float radius = 1f;
+
+    void Start()
+    {
+        radius = 1f;
+        mat.SetFloat("_Radius", radius);
+    }
+
+    void Update()
+    {
+        radius -= Time.deltaTime * 0.2f;
+        radius = Mathf.Clamp01(radius);
+        mat.SetFloat("_Radius", radius);
+    }
+}
